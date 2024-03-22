8f8e05ba64a3aafa5e2d5771e6fc0a3e277d8b98
Trindade12 <rm91825@estudante.fieb.edu.br>
2024-03-22 08:31:25 -0300

    Busca CEP

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ var nome = document.getElementById("nome");
 var email = document.getElementById("email");
 var tel = document.getElementById("tel");
 
+var cep = document.getElementById("cep");
 var logradouro = document.getElementById("logradouro");
 var numero = document.getElementById("numero");
 var complemento = document.getElementById("complemento");
@@ -17,9 +18,18 @@ var estado = document.getElementById("estado");
 function alertar(event){
     if(true){
         //alert("Você Clicou no Botão!!!" + "" + nome.value);
+
+        const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
+        fetch(url)
+        .then(resposta=>resposta.json())
+        .then(dados=>alert(dados.logradouro))
+
         saida.innerText = `Nome:  ${nome.value} 
                            Email: ${email.value}
-                           Telefone:  ${tel.value} 
+                           Telefone: ${tel.value}
+                           CEP: ${cep.value}
+                           Número: ${numero.value}
                            Logradouro: ${logradouro.value}
                            Complemento:  ${complemento.value} 
                            Bairro: ${bairro.value}
