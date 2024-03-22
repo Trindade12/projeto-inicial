7bef51de37db989cc08ba2ae1399802668777c1a
Trindade12 <rm91825@estudante.fieb.edu.br>
2024-03-22 08:14:14 -0300

    BootStrap e Alterações JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,30 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var saida = document.getElementById("saida-de-dados");
+
 var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var tel = document.getElementById("tel");
+
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
 function alertar(event){
     if(true){
-        alert("Você Clicou no Botão!!!" + "" + nome.value);
+        //alert("Você Clicou no Botão!!!" + "" + nome.value);
+        saida.innerText = `Nome:  ${nome.value} 
+                           Email: ${email.value}
+                           Telefone:  ${tel.value} 
+                           Logradouro: ${logradouro.value}
+                           Complemento:  ${complemento.value} 
+                           Bairro: ${bairro.value}
+                           Cidade:  ${cidade.value} 
+                           Estado: ${estado.value}
+                           `;
     }
 }
